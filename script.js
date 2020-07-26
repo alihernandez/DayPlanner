860bd0d7b38edfb90503758690e870e32f3cb82c
phantomramen <ali.hernandez9two@gmail.com>
2020-07-25 20:10:30 -0600

    local storage battle

--- script.js
+++ script.js
@@ -12,7 +12,10 @@
 //WHEN I refresh the page
 //THEN the saved events persist
 
+//render items 
+//let toDo = JSON.parse(localStorage.getItem('items')) || [];
 
+//const renderItems = (items) => {
 
 //curent date and time
 const m = moment();
@@ -22,20 +25,66 @@ displayMoment.textContent=m
 
 console.log(m.format("LLLL"));
 
+//this first part i worked through during office hours but i just felt it was too much and didn't really understand why 
+//if(localStorage.getItem("input") !== null) {
+   // toDoItem = JSON.parse(localStorage.getItem("input"));
+    //for(i=0; i < toDoItem.length; i++) {
+        //var hour = Object.keys(toDoItem[i])
+        //var text = toDoItem[i].hour
+        //console.log(hour)
+        //console.log(text)
+        //$(".description").each(function () {
+           //if( $(this).attr("id") === hour) {
+            //$(this).val(text);
+           //}
+        //})
+    //}
+//}
+//;
+
+//save to local storage
+$(".saveBtn").on("click", function() {
+    var timeOfDay= $(this).parent().attr("id");
+    var textContent= $("input").val().trim();
+
+    localStorage.setItem(timeOfDay, textContent);
+    console.log(timeOfDay, textContent);
+})
+
+
 //change colors depending on time
-const timeEl = $(".hour");
-timeEl.each((div => console.log($(this))));
+//const timeEl = $(".hour");
+//timeEl.each((div => console.log($(this))));
 
 
 
 //save user input to local storage
-$('.saveBtn').click(function() {
-    var str = $(this).siblings('.description').val();
 
-    localStorage.setItem("input.description", str);
+//$('.saveBtn').click
+//(function() {
+   //let text = $(this).siblings("input").val().trim();
+   //let hours =  $(this).siblings("input").attr("id");
+   //var object = {};
+   //object[hours] = text;
+    //console.log(object)
+    //toDoItem.push(object);
+    //console.log(toDoItem);
+    //localStorage.setItem("items", JSON.stringify(toDoItem));
+//}
+//)//;
 
-}
-)
+
+//pulling user info
+//$("#9AM").children("input").val(localStorage.getItem("9AM"));
+$("#10AM").children("input").val(localStorage.getItem("10AM"));
+$("#11AM").children("input").val(localStorage.getItem("11AM"));
+$("#12PM").children("input").val(localStorage.getItem("12PM"));
+$("#1PM").children("input").val(localStorage.getItem("1PM"));
+$("#2PM").children("input").val(localStorage.getItem("2PM"));
+$("#3PM").children("input").val(localStorage.getItem("3PM"));
+$("#4PM").children("input").val(localStorage.getItem("4PM"));
+$("#5PM").children("input").val(localStorage.getItem("5PM"));
+
 
 
 //if (hour > 9) {
